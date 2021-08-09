57a733b1a6d220a6ddc030cff55cd9e76b8e72ba
basilmaben <basilmaben.bm@gmail.com>
2021-08-09 18:58:54 +0530

    renamed file

---
+++ src/pages/ArticlesListPage.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import articleContent from './article-content'
+import ArticlesList from '../components/ArticlesList';
+
+const ArticlesListPage = () => {
+    return (
+        <>
+            <h1>Articles</h1>
+            <ArticlesList articles = {articleContent}/>
+            
+        </>
+            
+        
+    );
+}
+
+export default ArticlesListPage;
