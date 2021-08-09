f395b00a10f39f48807f0ad46bd906423bc6695a
basilmaben <basilmaben.bm@gmail.com>
2021-08-09 18:19:30 +0530

    Navbar imported

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
-import React, {Component} from 'react';
+import React /* {Component} */ from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import AboutPage from './pages/AboutPage';
 import ArticlesList from './pages/ArticlesList';
 import ArticlePage from './pages/ArticlePage';
+import NavBar from './NavBar';
 
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
    
       <Router>
          <div className="App">
+           <NavBar/>
            <div id ="page-body">
          <Route path = "/" component = {HomePage} exact/>
          <Route path = '/about' component = {AboutPage}/>
