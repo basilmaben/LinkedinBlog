b6ebe10fd104258a62799787c19e51ba32acd426
basilmaben <basilmaben.bm@gmail.com>
2021-08-13 18:37:57 +0530

    path added

--- my-blog-backend/src/server.js
+++ my-blog-backend/src/server.js
@@ -1,10 +1,12 @@
 import express from 'express';
 import bodyParser from 'body-parser'
 import { MongoClient } from 'mongodb'
-
+import path from 'path'
 /* const cors = require("cors"); */
 
 const app = express();
+
+app.use(express.static(path.join(__dirname, '/build')))
 
 app.use(bodyParser.json());
 
@@ -80,5 +82,9 @@ app.post('/api/articles/:name/add-comment', async (req, res) => {
        res.status(200).json(updatedArticleInfo);
    }, res);
 });
+
+app.get('*', (req, res)=>{
+  res.sendFile(path.join(__dirname  + '/build/index.html'))
+})
 
 app.listen(8000, () => console.log('Listening on port 8000'));
